0872c37b7e1084c84eb5f9fab7c9b78261062f5f
Waleed Hassan <waleed46shah@gmail.com>
2023-10-30 18:10:07 +0500

    Build a navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+function NavBar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize={16} />
+      <Text> Navbar</Text>
+    </HStack>
+  );
+}
+
+export default NavBar;
