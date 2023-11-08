82a2bd45cd4839e1ace9b4a50017771f1af3d8c7
Waleed Hassan <waleed46shah@gmail.com>
2023-11-08 14:37:16 +0500

    Show a spinner while fetching Genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,7 +6,7 @@ import GameCardContainer from "./GameCardContainer";
 
 function GameGrid() {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,11,12];
   return (
     <>
       {error && <Text color="red">{error}</Text>}
