ede62c7b76a8ce2b500b45af14405c89fe7a30d4
Waleed Hassan <waleed46shah@gmail.com>
2024-01-29 18:28:59 +0500

    Ship genres statically

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -18,10 +18,6 @@ interface Props {
 function GenreList({ selectedGenre, onSelectGenre }: Props) {
   const { data, isLoading, error } = useGenres();
 
-  if (error) return null;
-
-  if (isLoading) return <Spinner />;
-
   return (
     <>
       <Heading fontSize="2xl" marginBottom={3}>
